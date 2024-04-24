dc6f4591bbe4ad9148c6b97bb4b97f76aeebdf57
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-04-24 20:39:22 +0300

    Added a while true and delay so the script runs infinitely

--- psMonitor.py
+++ psMonitor.py
@@ -1,5 +1,4 @@
 import json
-import os
 import psutil
 import time
 from datetime import datetime
@@ -16,14 +15,17 @@ def to_dict(process_id):
 
 
 if __name__ == '__main__':
-    processes = psutil.pids()
+    while True:
+        processes = psutil.pids()
 
-    start = datetime.now()
-    with Pool() as pool:
-        results = pool.map(to_dict, processes)
-    process_list = [p for p in results if p is not None]
-    jsonFile = json.dumps(process_list, indent=4)
+        start = datetime.now()
+        with Pool() as pool:
+            results = pool.map(to_dict, processes)
+        process_list = [p for p in results if p is not None]
+        jsonFile = json.dumps(process_list, indent=4)
 
-    with open(f'jsons/process_list{round((time.time() + datetime.timestamp(start)) / 2)}.json', 'w') as outfile:
-        outfile.write(jsonFile)
-    print(f"Full processing took {datetime.now() - start}")
+        with open(f'jsons/process_list{round((time.time() + datetime.timestamp(start)) / 2)}.json', 'w') as outfile:
+            outfile.write(jsonFile)
+        print(f"Full processing took {datetime.now() - start}")
+
+        time.sleep(5)
